2313293f553796e5fe0dbbf66023cbc973e213cd
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-01-26 13:35:05 +0530

    still major tweaks needed

--- amazon.py
+++ amazon.py
@@ -7,8 +7,9 @@ account_sid = "AC5d88a649c63801497370cbd97a704697" #Your Twilio account ID
 auth_token = "539f1ccc3d82f482e52ca7e8cfd548e6"    #Your secret API Token
  
 client = TwilioRestClient(account_sid, auth_token)
-url = input('Enter the Url of Product Page')
-your_price = input('Enter the price your you want') 
+url = input('Enter the Url of Product Page: ')
+your_price = input('Enter the price your you want: ')
+contact_info = input('Your Contact Info: ')
 
 while True:
     page = urllib.request.urlopen(url)
@@ -17,7 +18,7 @@ while True:
     for string in current_price.stripped_strings:
         curr_price = repr(string)[1:-4]
     if(curr_price <= your_price): 
-        msg = client.messages.create(to="+917404663867", from_="+14843094282", body="rush to Amazon, Price of Headsets are below your price") #Will send SMS to your phone number
+        msg = client.messages.create(to=contact_info, from_="+14843094282", body="rush to Amazon, Price of Headsets are below your price") #Will send SMS to your phone number
         print("SMS Sent Thanks....quitting application")
         quit()
     else:
